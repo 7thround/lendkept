f05d2526c3e504e5f585839a67dcec74469270d4
supperrzz <hypperlabs@gmail.com>
2024-10-12 23:37:11 -0700

    span-col-2 for name

--- pages/[company]/apply.tsx
+++ pages/[company]/apply.tsx
@@ -119,11 +119,11 @@ const MortgageApplicationForm = ({ company, partner }: Props) => {
       </div>
       <div className="border-t border-gray-300 my-6"></div>
 
-      {/* Personal Info Section */}
+      {/* Applicant Info Section */}
       <div className="mt-6">
         <h2 className="text-lg font-semibold mb-4">Applicant Info</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="col-span-1">
+          <div className="col-span-2">
             <input
               placeholder="Full Name"
               type="text"
