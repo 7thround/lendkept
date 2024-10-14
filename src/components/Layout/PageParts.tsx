c6f57f51c09640a86b21071376b5347b4ec21cf1
supperrzz <hypperlabs@gmail.com>
2024-10-13 21:41:33 -0700

    fix cols

--- src/components/Layout/PageParts.tsx
+++ src/components/Layout/PageParts.tsx
@@ -10,6 +10,23 @@ export const Column = ({
   col: number;
 }) => (
   <>
+    <div
+      className={`
+    hidden
+    md:col-span-1
+    md:col-span-2
+    md:col-span-3
+    md:col-span-4
+    md:col-span-5
+    md:col-span-6
+    md:col-span-7
+    md:col-span-8
+    md:col-span-9
+    md:col-span-10
+    md:col-span-11
+    md:col-span-12
+    `}
+    ></div>
     <div className={`col-span-12 md:col-span-${col} space-y-4`}>{children}</div>
   </>
 );
