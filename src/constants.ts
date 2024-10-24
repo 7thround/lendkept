ce6c0f38d0a742c57d172c0b4f8b1224769ef019
supperrzz <hypperlabs@gmail.com>
2024-10-23 17:28:10 -0700

    update status names

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ import { LoanStatus } from "@prisma/client";
 
 export const LoanStatusLabels: { [key in keyof typeof LoanStatus]: string } = {
   POSSIBLE_LOAN: "Possible Loan",
-  APPLICATION_SUBMITTED: "Application Submitted",
-  CREDIT_AND_DOCUMENTS: "Credit and Documents",
+  APPLICATION_SUBMITTED: "Application and Credit Check",
+  CREDIT_AND_DOCUMENTS: "Documents and Processing",
   UNDERWRITING: "Underwriting",
   LOAN_FUNDED: "Loan Funded",
   ON_HOLD: "On Hold",
